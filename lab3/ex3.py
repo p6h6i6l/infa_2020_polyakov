292cc0b75cfc349fe073dfe1afba27b46e2ed7df
Samsonov Vladimir <samsonovv@MacBook-Pro-Samsonov.local>
2021-10-01 11:02:31 +0300

    после рефакторинга

--- lab3/ex3.py
+++ lab3/ex3.py
@@ -2,6 +2,9 @@ import pygame
 from pygame.draw import *
 
 pygame.init()
+FPS = 30
+screen = pygame.display.set_mode((1500, 800))
+tmp = pygame.display.set_mode((1500, 800))
 black = (0,0,0)
 blue = (0, 255,255)
 green =(154,205,50)
@@ -11,19 +14,20 @@ red = (255,0,0)
 grey = (216, 191, 216)
 lavender = (230,230,250)
 yellow = (255, 215, 0)
-pink= ( 212, 15, 252)
+pink = ( 212, 15, 252)
 
-def icecream(screen):
+def icecream():
     polygon(screen, yellow, [[280, 490], [280, 400], [190, 430]])
     circle(screen, red, (260, 390), 30)
     circle(screen, brown, (215, 410), 30)
     circle(screen, white, (230, 380), 30)
 
-
-def man(screen):
-    
+def background():
     rect(screen, blue, (0, 0, 1500, 400))
     rect(screen, green, (0, 400, 1500, 400))
+
+def man():
+
     ellipse(screen, grey, (400, 280, 170, 400))
     circle(screen, lavender, (485, 245), 70)
 
@@ -31,9 +35,8 @@ def man(screen):
     aalines(screen, black, False, [[450, 630], [430, 770],  [400, 780]])
     line(screen, black,  [270, 490], [440, 330], 1)
     line(screen, black,  [700, 490], [540, 330], 1)
-    
 
-def woman (screen):
+def woman ():
     line(screen, black,  [700, 490], [900, 330], 1)
     polygon(screen, pink, [[900, 320], [1000, 630], [800, 630]])
     circle(screen, white, (900, 270), 70)
@@ -41,28 +44,20 @@ def woman (screen):
     aalines(screen, black, False, [[950, 600], [950, 700],  [1000, 700]])
     aalines(screen, black, False, [[905, 350], [960, 400],  [1050, 350]])
 
-def baloon (screen):
+def baloon ():
     line(screen, black,  [1050, 400], [1060, 250], 1)
     polygon(screen, red, [[1060, 250], [1100, 200], [1020, 190]])
     circle(screen, red, (1045, 195), 25)
     circle(screen, red, (1080, 195), 25)
-    
-    
-    
 
-    
-FPS = 30
-screen = pygame.display.set_mode((1500, 800))
-tmp = pygame.display.set_mode((1500, 800))
+def all():
+    background()
+    man()
+    woman()
+    baloon()
+    icecream()
 
-
-man(screen)
-woman(screen)
-baloon(screen)
-icecream(screen)
-
-
-
+all()
 pygame.display.update()
 clock = pygame.time.Clock()
 finished = False
